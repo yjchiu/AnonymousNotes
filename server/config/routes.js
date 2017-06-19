37c97b9bf6c0b00d65f33c7a5c852ab075898378
Aaron <yjchiu1218@gmail.com>
2017-06-19 15:15:56 -0700

    fix routes path

--- server/config/routes.js
+++ server/config/routes.js
@@ -4,6 +4,6 @@ module.exports = function(app){
     app.get('/notes',notecontroller.index);
     app.post('/new',notecontroller.create);
     app.all('*', (req,res,next) => {
-        res.sendfile(path.resolve("./public/dist/index.html"))
+        res.sendfile(path.resolve("./../public/dist/index.html"))
     });
 }
